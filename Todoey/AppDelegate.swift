53bfc89129f1a474c8edbf19c9b8588c92d7c135
Dev-MuTTiNeeR <cemakkaya1312@gmail.com>
2026-03-16 12:09:36 -0300

    Added realm with full function and added date.

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -17,8 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        //print(Realm.Configuration.defaultConfiguration.fileURL)
+        
         do {
-            let realm = try Realm()
+            _ = try Realm()
         } catch {
             print("Error initialising new realm, \(error)")
         }
@@ -44,10 +46,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-    
-    func applicationWillTerminate(_ application: UIApplication) {
-        
     }
 }
 
